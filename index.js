2debe09141ad51483e68a4a438e85b79a38cca6f
fonsecasBruno <brunofsouza669@gmail.com>
2024-03-15 21:16:03 -0300

    start-mongodb

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+const routes = require('./routes/routes')
+app.use('/', routes)
+
+app.get('/', function(req, res){
+    res.send('Rota Principal')
+})
+
+app.listen(3000, function(){
+    console.log('Server UP port 3000')
+})
