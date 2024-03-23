cc8b29d3213b8a352cb47062e73df471ce60b5f5
fonsecasBruno <brunofsouza669@gmail.com>
2024-03-22 21:28:35 -0300

    add

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function main(){
 
 try {
-    await mongoose.connect('mongodb+srv://brunofonseca:@cluster0.dvywx9z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect(`mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PWD}@cluster0.o1asyjf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
     console.log('Banco ok');
 } catch (error) {
     console.log('Erro: ' + error)
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const Produto = require('../models/Produto');
 const router = express.Router()
 
 router.get('/', function(req, res){
+    res.json({})
+});
+
+router.get('/produtos', async function(req, res){
+    res.json(await Produto.find())
+});
+
+router.post('/produtos', async function(req, res){
+    res.json(await Produto.create({nome: 'Coca-Cola'}))
+});
+
+router.get('/produtos/:id', async function(req, res){
+    res.json(await Produto.findById(req.params.id))
+});
+
+router.delete('/produtos/:id', async function(req, res){
+    res.json(await Produto.findByIdAndDelete(req.params.id))
+});
+
+router.put('/produtos/:id', async function(req, res){
+    res.json(await Produto.findByIdAndUpdate(req.params.id, req.body))
+});
+
+router.get('/comanda', function(req, res){
     res.json({})
 });
 
